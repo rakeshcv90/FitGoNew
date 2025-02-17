ce9c88c5872caee55fcc06ee9f87b0db9f386d43
Sahil <sahilpahat@cvinfotech.com>
2025-02-17 11:42:19 +0530

    Working

--- react-native.config.js
+++ react-native.config.js
@@ -4,4 +4,11 @@ module.exports = {
       android: {},
     },
     assets: ['./assets/fonts'], // adjust the path based on your project structure
+    dependencies: {
+      'react-native-google-mobile-ads': {
+        platforms: {
+          ios: null, // Disable auto-linking for iOS, handle manually
+        },
+      },
+    },
   }; 
